93a7818b709b5cb3323c683dbee561ca0880bc7c
bharath <bharath.s@target.com>
2026-01-12 21:59:48 +0530

    [main] adding better chuck size.

--- src/config.py
+++ src/config.py
@@ -20,13 +20,13 @@ LLM_CACHE_PATH = "data/cache/llm_cache.db"
 RAW_DATA_PATH = "docs/"
 
 
-CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "420"))
-CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "100"))
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "800"))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "400"))
 CHUNK_METHOD = os.getenv("CHUNK_METHOD", "recursive")
 
 
-CHUNK_TABLE_SIZE = int(os.getenv("CHUNK_TABLE_SIZE", "2000"))
-CHUNK_TABLE_OVERLAP = int(os.getenv("CHUNK_TABLE_OVERLAP", "200"))
+CHUNK_TABLE_SIZE = int(os.getenv("CHUNK_TABLE_SIZE", "800"))
+CHUNK_TABLE_OVERLAP = int(os.getenv("CHUNK_TABLE_OVERLAP", "400"))
 
 
 RERANKER_TYPE = os.getenv("RERANKER_TYPE", "bi").lower()
